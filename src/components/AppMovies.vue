2c1d294c32166d7f89c9182f8d28c9fe10f61492
Milena <milenacuk89@gmail.com>
2018-12-04 19:06:53 +0100

    Zadatak 2

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,9 +1,45 @@
 <template>
-    
+    <div>
+        <table border='1'>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Director</th>
+                    <th>ImageUrl</th>
+                    <th>Duration</th>
+                    <th>Relase Data</th>
+                    <th>Genre</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for='movie in movies' :key='movie.id'>
+                    <td>{{ movie.title }}</td>
+                    <td>{{ movie.director }}</td>
+                    <td>{{ movie.imageUrl }}</td>
+                    <td>{{ movie.duration }}</td>
+                    <td>{{ movie.relaseData }}</td>
+                    <td>{{movie.genre }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
+import { movies } from '../services/Movies.js'
+
 export default {
-    
+    data(){
+        return{
+            movies:[]
+        }
+    },
+    created(){
+        movies.get().then(response => {
+            this.movies = response.data;
+        }).catch(error => {
+            console.log(error.response);
+        })
+    }
 }
 </script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,6 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-        <a class="navbar-brand" href="#">
-            <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
-        </a>
-    <a class="navbar-brand" href="#">Movies</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">        
+        <router-link class="navbar-brand" to="/movies">Movies</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
